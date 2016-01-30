80c26357e7901bfdd6f03d10554977fa3845c723
Mike Pastore <mike@oobak.org>
2016-01-30 14:56:45 -0600

    Add tests and Travis configuration

--- spec/dalli/rate_limiter_spec.rb
+++ spec/dalli/rate_limiter_spec.rb
@@ -1,11 +1,42 @@
-require 'spec_helper'
+require "spec_helper"
 
 describe Dalli::RateLimiter do
-  it 'has a version number' do
-    expect(Dalli::RateLimiter::VERSION).not_to be nil
+  Then { !Dalli::RateLimiter::VERSION.nil? }
+
+  Given(:lim) do
+    Dalli::RateLimiter.new nil,
+      :max_requests => 5, :period => 8, :key_prefix => RUBY_VERSION
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  context "with no previous attempts" do
+    When(:result) { lim.exceeded? "test_key_1" }
+
+    Then { !result }
+  end
+
+  context "with too many attempts" do
+    When(:result) { 6.times { lim.exceeded? "test_key_2" } }
+
+    Then { result && result > 0 }
+  end
+
+  context "with sleeping" do
+    When { sleep 6.times { lim.exceeded? "test_key_3" } }
+
+    When(:result) { lim.exceeded? "test_key_3" }
+
+    Then { !result }
+  end
+
+  context "with almost too many requests" do
+    When(:result) { lim.exceeded? "test_key_4", lim.max_requests }
+
+    Then { !result }
+  end
+
+  context "with too many requests" do
+    When(:result) { lim.exceeded? "test_key_5", lim.max_requests + 1 }
+
+    Then { result && result > 0 }
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,3 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'dalli/rate_limiter'
+$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+require "dalli/rate_limiter"
+require "rspec-given"
